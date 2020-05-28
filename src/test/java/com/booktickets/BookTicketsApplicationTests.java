ea34188a5d54ec0760353c85275959b8cb3d95f1
sneha <sneha.magdum11@gmail.com>
2020-05-27 23:02:27 -0700

    Spring boot initial setup

---
+++ src/test/java/com/booktickets/BookTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booktickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
