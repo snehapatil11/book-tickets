85f5bb9cd4f1c611680cebec548840f825ff6c6f
sneha <sneha.magdum11@gmail.com>
2020-06-02 22:38:08 -0700

    Added RDS MySQL database connection

--- src/main/java/com/booktickets/BookTicketsApplication.java
+++ src/main/java/com/booktickets/BookTicketsApplication.java
@@ -1,12 +1,8 @@
 package com.booktickets;
 
-import java.util.Arrays;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableJpaAuditing
